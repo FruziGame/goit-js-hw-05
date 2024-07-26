db08bec557c97cc729eb2e25c3598f088a9851cc
Hlib <grossogleb@gmail.coml>
2024-07-26 18:12:19 +0300

    donee

--- js/task-2.js
+++ js/task-2.js
@@ -2,20 +2,67 @@
 console.log("task-2")
 
 
-function calcAverageCalories(days) {
+const getUsersWithFriend = (users, friendName) => users.filter(user => user.friends.includes(friendName));
 
-   if (days.length === 0) {
-      return 0;
+
+
+const allUsers = [
+   {
+     name: "Moore Hensley",
+     friends: ["Sharron Pace"]
+   },
+   {
+     name: "Sharlene Bush",
+     friends: ["Briana Decker", "Sharron Pace"]
+   },
+   {
+     name: "Ross Vazquez",
+     friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"]
+   },
+   {
+     name: "Elma Head",
+     friends: ["Goldie Gentry", "Aisha Tran"]
+   },
+   {
+     name: "Carey Barr",
+     friends: ["Jordan Sampson", "Eddie Strong"]
+   },
+   {
+     name: "Blackburn Dotson",
+     friends: ["Jacklyn Lucas", "Linda Chapman"]
+   },
+   {
+     name: "Sheree Anthony",
+     friends: ["Goldie Gentry", "Briana Decker"]
    }
+ ];
+ 
+ console.log(getUsersWithFriend(allUsers, "Briana Decker")); 
+ // [
+ //   {
+ //     name: "Sharlene Bush",
+ //     friends: ["Briana Decker", "Sharron Pace"]
+ //   },
+ //   {
+ //     name: "Sheree Anthony",
+ //     friends: ["Goldie Gentry", "Briana Decker"]
+ //   }
+ // ]
+ 
+ console.log(getUsersWithFriend(allUsers, "Goldie Gentry"));
+ // [
+ //   {
+ //     name: "Elma Head",
+ //     friends: ["Goldie Gentry", "Aisha Tran"]
+ //   },
+ //   {
+ //     name: "Sheree Anthony",
+ //     friends: ["Goldie Gentry", "Briana Decker"]
+ //   }
+ // ]
+ 
+ console.log(getUsersWithFriend(allUsers, "Adrian Cross" )); // []
+ 
 
-   let totalCalories = 0
-
-   for (const item of days) {
-      totalCalories += item.calories
-   }
-
-   return totalCalories / days.length
-      
-}
 
 
